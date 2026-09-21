84dbb86e994c04bb6c78062435f57a72abb2436d
Rafioul Hasan <rafioulhasan2@gmail.com>
2026-09-21 10:10:10 +0600

    del: toggle theme removed from viewDetails

--- src/features/vocabulary/components/modals/VocabularyFullscreenModal.tsx
+++ src/features/vocabulary/components/modals/VocabularyFullscreenModal.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { createPortal } from "react-dom";
-import { ThemeToggle } from "@/components/shared";
 import { PartOfSpeech, VocabularyStatus } from "@/types";
 import {
   MyVocabularyItem,
